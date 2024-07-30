6c641830e320d76d31011b5fbc82b400af4b20de
RevChem <131515178+RevChem@users.noreply.github.com>
2024-07-30 04:03:27 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+token = '7272724544:AAEUEP0qs6LsRDcBVM8cSMAjiR2cNMSqiiQ'
+users=['6334851806', '181122762']
+lastimage='/var/lib/motion/lastsnap.jpg'
+motiondir='/var/lib/motion'
---
+++ sender.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+while true; do
+    python3 /home/devyati/bot/sender.py
+    sleep 30
+done
